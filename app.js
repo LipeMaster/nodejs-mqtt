528c7eaf44bbe568016a7d36b2b55fe86b4f530c
Felipe PC-Meta <felipe.fernandes@meta.com.br>
2019-04-11 15:27:52 -0300

    mqtt conectando a serviço na nuvem MQTT CLOUD

--- app.js
+++ app.js
@@ -1,8 +1,17 @@
 const mqtt = require('mqtt');
-const mqttBroker = '192.168.2.10';
-const client = mqtt.connect(mqttBroker);
+
+const optionsBroker = {
+    port: 11134,
+    host: 'mqtt://m16.cloudmqtt.com',
+    clientId: 'nodeJS',
+    username: 'wnwkuccv',
+    password: 'wpKlxge1zyLK',
+    encoding: 'utf8'
+}
+const client = mqtt.connect('mqtt://m16.cloudmqtt.com', optionsBroker);
 
 client.on('connect', () => {
+    console.log('Connected')
     client.subscribe('presence', (err) => {
         if (!err) {
             client.publish('presence', 'Hello mqtt broker')
@@ -13,5 +22,5 @@ client.on('connect', () => {
 client.on('message', (topic, message) =>{
     //message is buffer
     console.log(message.toString());
-    client.end()
+    //client.end()
 });
